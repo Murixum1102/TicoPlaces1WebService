5b3e700c28dba362362b1561f8ed50f4974a9c24
Murixum1102 <muripsp@gmail.com>
2019-04-14 01:00:04 -0600

    Updated PlaceRestController with UploadFileService
    +IUploadFileService
    +Implementacion UploadFileService
    +Actualización PlaceRestController con UploadFileService

--- src/main/java/com/ulatina/ticoplaces/webservice/controllers/PlaceRestController.java
+++ src/main/java/com/ulatina/ticoplaces/webservice/controllers/PlaceRestController.java
@@ -1,20 +1,12 @@
 package com.ulatina.ticoplaces.webservice.controllers;
-import java.io.File;
 import java.io.IOException;
 import java.net.MalformedURLException;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.UUID;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.io.Resource;
-import org.springframework.core.io.UrlResource;
 import org.springframework.dao.DataAccessException;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
@@ -38,6 +30,7 @@ import com.ulatina.ticoplaces.webservice.model.entity.District;
 import com.ulatina.ticoplaces.webservice.model.entity.Place;
 import com.ulatina.ticoplaces.webservice.model.entity.Province;
 import com.ulatina.ticoplaces.webservice.model.services.IPlaceService;
+import com.ulatina.ticoplaces.webservice.model.services.IUploadFileService;
 
 @CrossOrigin(origins = {"http://localhost:4200"})
 @RestController
@@ -47,7 +40,9 @@ public class PlaceRestController {
 	@Autowired
 	private IPlaceService placeService;
 	
-	private final Logger log = LoggerFactory.getLogger(PlaceRestController.class);
+	@Autowired
+	private IUploadFileService uploadService;
+	
 	
 	@GetMapping("/places")
 	public List<Place> index(){
@@ -91,14 +86,7 @@ public class PlaceRestController {
 			Place place = placeService.findById(id);
 			String nombreFoto = place.getPhoto();
 			//Si la foto existe se borra al borrar el Place
-			if(nombreFoto !=null && nombreFoto.length() > 0) {
-				//se debe cambiar este path cuando se amarre al front y se haga deployment
-				Path rutaFoto = Paths.get("uploads").resolve(nombreFoto).toAbsolutePath();
-				File archivoFoto = rutaFoto.toFile();
-				if(archivoFoto.exists() && archivoFoto.canRead()) {
-					archivoFoto.delete();
-				}
-			}
+			uploadService.eliminar(nombreFoto);
 			
 			placeService.delete(id);
 		}catch(DataAccessException e) {
@@ -141,14 +129,12 @@ public class PlaceRestController {
 		Place place = placeService.findById(id);
 		
 		if(!archivo.isEmpty()) {
-			String nombreArchivo = UUID.randomUUID().toString() + "_" + archivo.getOriginalFilename().replace(" ", "");
-			//Ruta absoluta desde donde se van a subir las imagenes. Se debe cambiar al unir con FrontEnd
-			Path rutaArchivo = Paths.get("uploads").resolve(nombreArchivo).toAbsolutePath();
-			log.info(rutaArchivo.toString());
+			
+			String nombreArchivo = null;
 			try {
-				Files.copy(archivo.getInputStream(), rutaArchivo);
+				nombreArchivo = uploadService.copiar(archivo);
 			} catch (IOException e) {
-				response.put("mensaje", "Error uploading picture: " + nombreArchivo);
+				response.put("mensaje", "Error uploading picture: ");
 				response.put("error", e.getMessage().concat(": ").concat(e.getCause().getMessage()));
 				return new ResponseEntity<Map<String, Object>>(response, HttpStatus.INTERNAL_SERVER_ERROR);
 			}
@@ -168,28 +154,15 @@ public class PlaceRestController {
 	@GetMapping("/uploads/img/{nombreFoto:.+}")
 	public ResponseEntity<Resource> verFoto(@PathVariable String nombreFoto){
 		
-		Path rutaArchivo = Paths.get("uploads").resolve(nombreFoto).toAbsolutePath();
-		log.info(rutaArchivo.toString());
 		Resource recurso = null;
 		
 		try {
-			recurso = new UrlResource(rutaArchivo.toUri());
+			recurso = uploadService.cargar(nombreFoto);
 		} catch (MalformedURLException e) {
 			
 			e.printStackTrace();
 		}
 		
-		if(!recurso.exists() && !recurso.isReadable()) {
-			rutaArchivo = Paths.get("src/main/resources/static/images").resolve("places.png").toAbsolutePath();
-			try {
-				recurso = new UrlResource(rutaArchivo.toUri());
-			} catch (MalformedURLException e) {
-				
-				e.printStackTrace();
-			}
-			log.error( "No se pudo cargar la imagen: " + nombreFoto);
-			
-		}
 		HttpHeaders header = new HttpHeaders();
 		header.add(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=\"" + recurso.getFilename() + "\"");
 		
