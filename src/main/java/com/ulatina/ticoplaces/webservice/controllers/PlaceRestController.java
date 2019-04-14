3f589e0bba173d910f4faa9833935cc00fe9e9c8
Murixum1102 <muripsp@gmail.com>
2019-04-13 21:15:25 -0600

    Place RestController Photo Upload
    +Post verb for single photo as MultipartFile
    +Added photo atribute on Place class (String).

--- src/main/java/com/ulatina/ticoplaces/webservice/controllers/PlaceRestController.java
+++ src/main/java/com/ulatina/ticoplaces/webservice/controllers/PlaceRestController.java
@@ -1,8 +1,15 @@
 package com.ulatina.ticoplaces.webservice.controllers;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -11,8 +18,10 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
 
 import com.ulatina.ticoplaces.webservice.model.entity.Canton;
 import com.ulatina.ticoplaces.webservice.model.entity.Category;
@@ -84,5 +93,34 @@ public class PlaceRestController {
 	@ResponseStatus(HttpStatus.OK)
 	public List<Category> findAllCategory() {
 		return placeService.findAllCategories();
+	}
+	
+	@PostMapping("places/upload")
+	public ResponseEntity<?> uploadPicture(@RequestParam("archivo") MultipartFile archivo, @RequestParam("id") Long id){
+		Map<String, Object> response = new HashMap<>();
+		
+		Place place = placeService.findById(id);
+		
+		if(!archivo.isEmpty()) {
+			String nombreArchivo = archivo.getOriginalFilename();
+			//Ruta absoluta desde donde se van a subir las imagenes. Se debe cambiar al unir con FrontEnd
+			Path rutaArchivo = Paths.get("uploads").resolve(nombreArchivo).toAbsolutePath();
+			
+			try {
+				Files.copy(archivo.getInputStream(), rutaArchivo);
+			} catch (IOException e) {
+				
+				return new ResponseEntity<Map<String, Object>>(response, HttpStatus.INTERNAL_SERVER_ERROR);
+			}
+			place.setPhoto(nombreArchivo);
+			placeService.save(place);
+			
+			response.put("place", place);
+			response.put("mensaje", "Photo uploaded correctly! Name: " + nombreArchivo);
+		}	
+		
+		
+		return new ResponseEntity<Map<String, Object>>(response, HttpStatus.CREATED);
+		
 	}
 }
