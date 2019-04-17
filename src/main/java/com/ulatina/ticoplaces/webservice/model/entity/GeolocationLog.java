62ae2c2f6b75af4bd83b2a2bb6d11cf538dea889
Murixum1102 <muripsp@gmail.com>
2019-04-16 18:53:12 -0600

    Update GeolocationLog.java

--- src/main/java/com/ulatina/ticoplaces/webservice/model/entity/GeolocationLog.java
+++ src/main/java/com/ulatina/ticoplaces/webservice/model/entity/GeolocationLog.java
@@ -1,5 +1,85 @@
 package com.ulatina.ticoplaces.webservice.model.entity;
 
-public class GeolocationLog {
+import java.io.Serializable;
+import java.time.Instant;
+import java.time.temporal.ChronoUnit;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.PrePersist;
+import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+
+
+@Entity
+@Table(name="geolocation")
+public class GeolocationLog implements Serializable{
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	private Long id;
+	private User user;
+	private String latitud;
+	private String longitud;
+	
+	@Column(name="log_time")
+	@Temporal(TemporalType.TIMESTAMP)
+	private Instant dateTime;
+	
+	@PrePersist
+	public void prePersist() {
+		dateTime = Instant.now().truncatedTo(ChronoUnit.SECONDS);
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public User getUser() {
+		return user;
+	}
+
+	public void setUser(User user) {
+		this.user = user;
+	}
+
+	public String getLatitud() {
+		return latitud;
+	}
+
+	public void setLatitud(String latitud) {
+		this.latitud = latitud;
+	}
+
+	public String getLongitud() {
+		return longitud;
+	}
+
+	public void setLongitud(String longitud) {
+		this.longitud = longitud;
+	}
+
+	public Instant getDateTime() {
+		return dateTime;
+	}
+
+	public void setDateTime(Instant dateTime) {
+		this.dateTime = dateTime;
+	}
+	
+	
+	
 }
